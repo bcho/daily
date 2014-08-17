868e9434a9e48c3b71dbb4185101de3140ea430e
GoneWithSin <liubingyao@gmail.com>
2014-08-17 14:19:07 +0800

    Add backend.

---
+++ theme/backend/js/script.js
@@ -0,0 +1,38 @@
+/**
+ * 
+ * 实现上传图片功能
+ * 
+ */
+$(document).ready(function(){
+  // upload image
+  $("#upload-img").change(function() {
+    // create temporary url
+    var tmppath = URL.createObjectURL(event.target.files[0]);
+    var img_address = $(this).val();
+    console.log(img_address);
+    // if upload image
+    if (img_address!== null) {
+      // set background, show image
+      $("#background").css("background-image", 'url("' + tmppath + '")');
+      $("#image").removeClass("hide");
+      $("#image").addClass("show");
+      // change font color
+      $("#editable").addClass("darken");
+    }
+  });
+  // delete image
+  $("#btn-delete").click(function(e) {
+    e.preventDefault();
+    var img_address = $("#upload-img").val();
+    // if image exists
+    if (img_address!== null) {
+      // hide image
+      $("#image").removeClass("show");
+      $("#image").addClass("hide");
+      // change font color
+      $("#editable").removeClass("darken");
+      // clear image address
+      $("#upload-img").val(null);
+    }
+  });
+});
